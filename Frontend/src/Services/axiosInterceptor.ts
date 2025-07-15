6ae21d524625cbb65e96b0315f03e5915aaa7bea
Asif Ali <aali9548702@gmail.com>
2025-07-15 17:23:42 +0530

    feat: add axios interceptor for handling API requests and responses

---
+++ Frontend/src/Services/axiosInterceptor.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+// Create axios instance
+//
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL_DEV}`,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
+
+// Request interceptor 
+api.interceptors.request.use(
+  (config) => {
+    console.log("config request ", config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+api.interceptors.response.use(
+  (response) => {
+    console.log("response ", response);
+    return response;
+  },
+  async (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
