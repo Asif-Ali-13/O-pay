f0f612502bdcc2d2700fc6c8d9c580fd38f47413
Asif Ali <aali9548702@gmail.com>
2025-07-16 11:57:37 +0530

    fix: baseURL

--- Frontend/src/Services/axiosInterceptor.ts
+++ Frontend/src/Services/axiosInterceptor.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create axios instance
 //
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL_PROD}`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL_PROD}/api/v1`,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
