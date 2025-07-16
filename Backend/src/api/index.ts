ef4c3b7ea325d73a1399022a69fa00d320b1d0e5
Asif Ali <aali9548702@gmail.com>
2025-07-16 22:05:01 +0530

    added the logic to connect with db in api/index.ts

--- Backend/src/api/index.ts
+++ Backend/src/api/index.ts
@@ -1,4 +1,23 @@
 import app from "../app";
 import serverless from "serverless-http";
+import { connectDB } from "../db/index";
 
-export const handler = serverless(app);
+let isConnected = false;
+
+const wrapper = async () => {
+  if (!isConnected) {
+    try {
+      await connectDB();
+      isConnected = true;
+      console.log("[Vercel] Connected to MongoDB ✅");
+    } catch (err) {
+      console.error("[Vercel] DB connection failed ❌", err);
+    }
+  }
+  return serverless(app);
+};
+
+export const handler = async (event: any, context: any) => {
+  const handlerFn = await wrapper();
+  return handlerFn(event, context);
+};
