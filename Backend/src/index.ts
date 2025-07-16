d7f111b3e52f8628138bedffdbd2cf5003f1f7e1
Asif Ali <aali9548702@gmail.com>
2025-07-16 21:04:52 +0530

    installed serverless handler and configure it

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,16 +1,23 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env"});
+dotenv.config({ path: "./.env" });
 
 import app from "./app";
 import { connectDB } from "./db/index";
 
-connectDB()
-  .then(() => {
-		app.listen(process.env.PORT || 8000, () => {
-			console.log(`Backend is listening on PORT : ${process.env.PORT}`);
-		});
-	})
-	.catch((error: Error) => {
-		console.log(`Error while connecting to DB: ${error}`);
-		process.exit(1);
-	})
+const PORT = process.env.PORT || 8000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Backend is listening on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Error while connecting to DB:`, error);
+    process.exit(1);
+  }
+};
+
+if (process.env.NODE_ENV !== "production") {
+  startServer();
+}
