8aa395f45c9921da29daa75680c5a11a8bc790b4
Asif Ali <aali9548702@gmail.com>
2025-07-16 11:34:33 +0530

    added the prod_url

--- Frontend/src/Services/axiosInterceptor.ts
+++ Frontend/src/Services/axiosInterceptor.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create axios instance
 //
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL_DEV}`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL_PROD}`,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
